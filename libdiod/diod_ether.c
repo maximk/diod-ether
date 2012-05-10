ea0ab84f3bd258195cc96df3c374f95eb53b6b1f
Maxim Kharchenko <maxim.kharchenko@gmail.com>
2012-05-10 04:10:05 +0300

    connecting existing interfaces added

--- libdiod/diod_ether.c
+++ libdiod/diod_ether.c
@@ -34,8 +34,11 @@
 
 struct diod_ether_t {
 	int nl_sock;
+	int existing_connected;
 	int try;
 };
+
+static void connect_existing_interfaces(Npsrv *srv, diod_ether_t *ether);
 
 diod_ether_t *diod_ether_create(void)
 {
@@ -67,7 +70,8 @@ diod_ether_listen(diod_ether_t *ether)
 
 	if (bind(ether->nl_sock, (struct sockaddr *)&saddr, sizeof(saddr)) < 0)
 		err_exit("bind netlink socket failed");
-
+	
+	ether->existing_connected = 0;
 	ether->try = 0;
 
 	return 0;
@@ -82,6 +86,9 @@ diod_ether_shutdown(diod_ether_t *ether)
 void
 diod_ether_accept_one(Npsrv *srv, diod_ether_t *ether)
 {
+	if (!ether->existing_connected)
+		connect_existing_interfaces(srv, ether);
+
 	uint8_t buf[4096];
 	int len = recv(ether->nl_sock, buf, sizeof(buf), 0);
 	if (len < 0)
@@ -152,6 +159,124 @@ diod_ether_accept_one(Npsrv *srv, diod_ether_t *ether)
 
 		hdr = NLMSG_NEXT(hdr, len);
 	}
+}
+
+static void connect_existing_interfaces(Npsrv *srv, diod_ether_t *ether)
+{
+	ether->existing_connected = 1;	// only try once
+
+	int sock = socket(AF_NETLINK, SOCK_DGRAM, NETLINK_ROUTE);
+	if (sock < 0)
+		return;
+
+	struct sockaddr_nl saddr = {
+		.nl_family = AF_NETLINK,
+		.nl_pid = ~getpid(),	// getpid() already taken
+		.nl_groups = 0,
+	};
+
+	if (bind(sock, (struct sockaddr *)&saddr, sizeof(saddr)) < 0)
+		goto error;
+
+	typedef struct nl_req_s nl_req_t;
+	struct nl_req_s {
+		struct nlmsghdr hdr;
+	    struct rtgenmsg gen;
+	};
+
+	struct sockaddr_nl kernel = {
+		.nl_family = AF_NETLINK,
+		.nl_pid = 0,
+		.nl_groups = 0,
+	};
+	nl_req_t req;
+	req.hdr.nlmsg_len = NLMSG_LENGTH(sizeof(struct rtgenmsg));
+	req.hdr.nlmsg_type = RTM_GETLINK;
+	req.hdr.nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP;
+	req.hdr.nlmsg_seq = 1;
+	req.hdr.nlmsg_pid = getpid();
+	req.gen.rtgen_family = AF_PACKET; /*  no preferred AF, we will get *all* interfaces */
+
+	struct iovec io = {
+		.iov_base = &req,
+		.iov_len = req.hdr.nlmsg_len,
+	};
+
+	struct msghdr rtnl_msg = {
+		.msg_iov = &io,
+		.msg_iovlen = 1,
+		.msg_name = &kernel,
+		.msg_namelen = sizeof(kernel),
+	};
+
+	if (sendmsg(sock, &rtnl_msg, 0) < 0)
+		goto error;
+
+	uint8_t reply[8192];
+	struct msghdr rtnl_reply;    /* generic msghdr structure */
+
+	io.iov_base = reply;
+    io.iov_len = sizeof(reply);
+    rtnl_reply.msg_iov = &io;
+    rtnl_reply.msg_iovlen = 1;
+    rtnl_reply.msg_name = &kernel;
+    rtnl_reply.msg_namelen = sizeof(kernel);
+
+    int len = recvmsg(sock, &rtnl_reply, 0); /* read lots of data */
+	if (len < 0)
+		goto error;
+
+	struct nlmsghdr *hdr = (struct nlmsghdr *)reply;
+	while (NLMSG_OK(hdr, len))
+	{
+		struct ifinfomsg *ifi = NLMSG_DATA(hdr);
+
+		if (hdr->nlmsg_type == RTM_NEWLINK &&
+				(ifi->ifi_flags & IFF_UP) != 0 &&
+				(ifi->ifi_flags & IFF_RUNNING) != 0)
+		{
+			// assume no padding between ifi and the first attribute
+			struct rtattr *rta = (void *)ifi + sizeof(*ifi);
+			int rta_len = len -sizeof(*ifi);
+
+			int ifname_ok = 0;
+			char *ifname = 0;
+			while (RTA_OK(rta, rta_len))
+			{
+				if (rta->rta_type == IFLA_IFNAME)
+				{
+					ifname = RTA_DATA(rta);
+					ifname_ok = ifname[0] == 'x' &&
+								ifname[1] == 'e' &&
+								ifname[2] == 'n';
+				}
+				rta = RTA_NEXT(rta, rta_len);
+			}
+			
+			if (ifname_ok)
+			{
+				msg("ether: existing link %s added", ifname);
+				Nptrans *trans = np_ethertrans_create(ifi->ifi_index);
+				if (trans != 0)
+				{
+					Npconn *conn = np_conn_create(srv, trans, "ether");
+					np_srv_add_conn(srv, conn);
+					ether->try = 0;
+				}
+				else
+					errn(np_rerror(), "np_ethertrans_create failed");
+			}
+		}
+
+		hdr = NLMSG_NEXT(hdr, len);
+	}
+
+	close(sock);
+	return;
+
+error:
+	close(sock);
+	errn(np_rerror(), "connect existing interfaces failed");
 }
 
 //EOF
