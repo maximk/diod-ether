ce338e86766e0b4af8c082e10b67e827e70191b6
Maxim Kharchenko <maxim.kharchenko@gmail.com>
2012-06-09 22:59:24 +0300

    basic net works

--- libnpfs/net.c
+++ libnpfs/net.c
@@ -22,11 +22,14 @@
 #include <sys/socket.h>
 
 #include <arpa/inet.h>
+#include <poll.h>
 
 #include "9p.h"
 #include "npfs.h"
 #include "xpthread.h"
 #include "npfsimpl.h"
+
+#define POLL_TIMEOUT	5000
 
 static Npfile *make_node(Npfile *parent, char *name, u8 type, np_file_vtab_t *vtab);
 static void destroy_node(Npfile *file);
@@ -34,7 +37,10 @@ static int next_inum (void);
 
 static int tcp_ctl_read(Npfile *file, u64 offset, u8 *data, u32 count);
 static int tcp_ctl_write(Npfile *file, u64 offset, u8 *data, u32 count);
+static int tcp_data_read(Npfile *file, u64 offset, u8 *data, u32 count);
+static int tcp_data_write(Npfile *file, u64 offset, u8 *data, u32 count);
 static int tcp_clone_open(Npfid *fid, int mode);
+static int tcp_listen_open(Npfid *fid, int mode);
 static void tcp_sock_cleanup(Npfile *ctlfile);
 
 np_file_vtab_t tcp_gen_vtab = { 0 };
@@ -52,9 +58,14 @@ np_file_vtab_t tcp_ctl_vtab = {
 	.write = tcp_ctl_write,
 };
 
-np_file_vtab_t tcp_data_vtab = { 0 };
+np_file_vtab_t tcp_data_vtab = {
+	.read = tcp_data_read,
+	.write = tcp_data_write
+};
 
-np_file_vtab_t tcp_listen_vtab = { 0 };
+np_file_vtab_t tcp_listen_vtab = {
+   .open = tcp_listen_open,
+};
 
 int np_file_open(Npfile *file, Npfid *fid, int mode)
 {
@@ -209,6 +220,7 @@ static int tcp_ctl_read(Npfile *file, u64 offset, u8 *data, u32 count)
 #define CTL_STATE_CREATED		0
 #define CTL_STATE_CONNECTED		1
 #define CTL_STATE_LISTENING		2
+#define CTL_STATE_ACCEPTED		3
 
 static int tcp_ctl_write(Npfile *file, u64 offset, u8 *data, u32 count)
 {
@@ -219,7 +231,7 @@ static int tcp_ctl_write(Npfile *file, u64 offset, u8 *data, u32 count)
 	if (file->state != CTL_STATE_CREATED)
 		goto error1;
 
-	Npfile *tcpdir = file->parent;
+	Npfile *sockdir = file->parent;
 
 	if (strncmp((char *)data, CTL_PREF_CONNECT, CTL_PREF_CONNECT_LEN) == 0)
 	{
@@ -247,25 +259,53 @@ static int tcp_ctl_write(Npfile *file, u64 offset, u8 *data, u32 count)
 			.sin_port = htons(port),
 		};
 
-		int n = connect(tcpdir->sock, (struct sockaddr *)&sa, sizeof(sa));
+		int n = connect(sockdir->sock, (struct sockaddr *)&sa, sizeof(sa));
 		if (n < 0) {
 			np_uerror(errno);
 			return -1;
 		}
 
-		Npfile *datafile = make_node(tcpdir, "data", P9_QTFILE, &tcp_data_vtab);
+		Npfile *datafile = make_node(sockdir, "data", P9_QTFILE, &tcp_data_vtab);
 		if (datafile == 0) {
 			np_uerror(ENOMEM);
 			return -1;
 		}
 
-		printf("~~~ sock %d connected to %s:%d\n", tcpdir->sock, pad, port);
+		printf("~~~ sock %d connected to %s:%d\n", sockdir->sock, pad, port);
 		file->state = CTL_STATE_CONNECTED;
 	}
 	else if (strncmp((char *)data, CTL_PREF_ANNOUNCE, CTL_PREF_ANNOUNCE_LEN) == 0)
 	{
-		//TODO
-		goto error2;
+		char *p = (char *)data +CTL_PREF_ANNOUNCE_LEN;
+		int port = atoi(p);
+		if (port == 0)
+			goto error1;
+
+		struct sockaddr_in sa = {
+			.sin_family = AF_INET,
+			.sin_addr.s_addr = htonl(INADDR_ANY),
+			.sin_port = htons(port),
+		};
+
+		if (bind(sockdir->sock, (struct sockaddr *)&sa, sizeof(sa)) < 0) {
+			np_uerror(errno);
+			return -1;
+		}
+		printf("~~~ sock %d bound to port %d\n", sockdir->sock, port);
+
+		Npfile *listenfile = make_node(sockdir, "listen", P9_QTFILE, &tcp_listen_vtab);
+		if (listenfile == 0) {
+			np_uerror(ENOMEM);
+			return -1;
+		}
+
+		if (listen(sockdir->sock, 8) < 0) {
+			np_uerror(errno);
+			return -1;
+		}
+
+		printf("~~~ sock %d listening\n", sockdir->sock);
+		file->state = CTL_STATE_LISTENING;
 	}
 	else
 		goto error1;
@@ -281,6 +321,45 @@ error1:
 	return 0;
 }
 
+static int tcp_data_read(Npfile *file, u64 offset, u8 *data, u32 count)
+{
+	Npfile *sockdir = file->parent;
+
+	struct pollfd pfd = {
+		.fd = sockdir->sock,
+		.events = POLLIN,
+	};
+
+	int s = poll(&pfd, 1, POLL_TIMEOUT);
+	if (s == 0) {
+		np_uerror(EAGAIN);
+		return -1;
+	}
+
+	int n = recv(sockdir->sock, data, count, 0);
+	if (n < 0) {
+		np_uerror(errno);
+		return -1;
+	}
+
+	printf("~~~ %d byte(s) read from sock %d\n", n, sockdir->sock);
+	return n;
+}
+
+static int tcp_data_write(Npfile *file, u64 offset, u8 *data, u32 count)
+{
+	Npfile *sockdir = file->parent;
+
+	int n = send(sockdir->sock, data, count, 0);
+	if (n < 0) {
+		np_uerror(errno);
+		return -1;
+	}
+
+	printf("~~~ %d byte(s) written to sock %d\n", n, sockdir->sock);
+	return n;
+}
+
 static int tcp_clone_open(Npfid *fid, int mode)
 {
 	Npfile *clonefile = fid->aux;
@@ -291,6 +370,7 @@ static int tcp_clone_open(Npfid *fid, int mode)
 		np_uerror(errno);
 		return -1;
 	}
+
 	char name[32];
 	snprintf(name, sizeof(name), "%d", sock);
 	Npfile *sockdir = make_node(tcpdir, name, P9_QTDIR, &tcp_sock_vtab);
@@ -310,6 +390,56 @@ error2:
 	destroy_node(sockdir);
 error1:
 	close(sock);
+	np_uerror(ENOMEM);
+	return -1;
+}
+
+static int tcp_listen_open(Npfid *fid, int mode)
+{
+	Npfile *listenfile = fid->aux;
+	Npfile *sockdir = listenfile->parent;
+	Npfile *tcpdir = sockdir->parent;
+
+	struct pollfd pfd = {
+		.fd = sockdir->sock,
+		.events = POLLIN,
+	};
+
+	int n = poll(&pfd, 1, POLL_TIMEOUT);
+	if (n == 0) {
+		np_uerror(EAGAIN);
+		return -1;
+	}
+
+	int newsock = accept(sockdir->sock, 0, 0);
+	if (newsock < 0) {
+		np_uerror(errno);
+		return -1;
+	}
+
+	char name[32];
+	snprintf(name, sizeof(name), "%d", newsock);
+	Npfile *newdir = make_node(tcpdir, name, P9_QTDIR, &tcp_sock_vtab);
+	if (newdir == 0)
+		goto error1;
+	newdir->sock = newsock;
+	Npfile *ctlfile = make_node(newdir, "ctl", P9_QTFILE, &tcp_ctl_vtab);
+	if (ctlfile == 0)
+		goto error2;
+	ctlfile->state = CTL_STATE_ACCEPTED;
+	Npfile *datafile = make_node(newdir, "data", P9_QTFILE, &tcp_data_vtab);
+	if (datafile == 0)
+		goto error3;
+
+	fid->aux = datafile;
+	return 0;
+
+error3:
+	destroy_node(ctlfile);
+error2:
+	destroy_node(newdir);
+error1:
+	close(newsock);
 	np_uerror(ENOMEM);
 	return -1;
 }
